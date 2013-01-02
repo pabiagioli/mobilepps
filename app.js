9b5d55e7b32f49f2ce844155d4487805539bdf27
Pablo Biagioli <pabiagioli@gmail.com>
2013-01-01 22:17:27 -0300

    initial commit with First approach it has the first two form subsections DAO access from the main app.js likely to change Libraries included node packages excluded

---
+++ app.js
@@ -0,0 +1,133 @@
+var mongoose = require('mongoose'),
+	express = require('express'),
+	socketio = require('socket.io'),
+	path = require('path'),
+	//nodemon = require('nodemon'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
+	ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
+	app = express();
+
+/* My Modules and Classes */
+/*var FormLabel = require("./model/Patient");*/
+
+
+/* DB Start with DAO */
+var DAOimpl = function(url,dbname,dbuser,dbpass){
+
+	var connStr ='mongodb://'; 
+	
+	if(dbuser != null || dbpass != null)
+		connStr += dbuser+':'+dbpass+'@'+url+'/'+dbname;
+	else
+		connStr += url+'/'+dbname;
+	
+	console.log("Connecting to %s ...\n",connStr);
+	
+	return mongoose.connect(connStr, function (err) {
+		if (err) {
+			console.log('Connection to MongoDB error');
+			return err;
+		}
+		console.log("Connection to MongoDB successful");
+  		// ok we're set
+	});
+	
+};
+
+var DAO = new DAOimpl('localhost', 'nodepps',null,null);
+
+/* 
+******************************
+*
+* Server and Passport config *
+*
+******************************
+*/
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'secret' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.engine('jade', require('jade').__express);
+});
+
+passport.serializeUser(function(user, done) {
+      done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+      done(null, obj);
+});
+
+passport.use(new LocalStrategy( 
+	function(username,password,done){
+		if(username == null)
+			console.log("error ocurred during auth");
+
+		/*QA.find({}, function(err,docs){
+			if (err){
+				console.log(err);
+			}else{
+		*/		
+				return done(null,{user:username});
+		/*	}
+		});*/
+		
+	}
+	));
+
+
+
+app.get('/',ensureLoggedIn('/login'), function(req,res){
+	/*QA.find({}, function(err,docs){
+		if (err){
+			console.log(err);
+		}else{*/
+			res.render('index.jade',{username:req.user/*, qa: docs*/});
+		/*}
+	});	*/
+});
+
+app.post('/',ensureLoggedIn('/login'), function(req,res){
+	/*QA.find({}, function(err,docs){
+		if (err){
+			console.log(err);
+		}else{*/
+			res.render('index.jade',{username:req.user/*, qa: docs*/});
+		/*}
+	});*/
+});
+
+app.get('/login',function(req,res){
+	res.render('login.jade', {username:req.user});
+});
+
+app.post('/login', 
+	passport.authenticate('local',{ failureRedirect: '/login'}),
+	function(req,res){
+		res.redirect('/');
+});
+
+
+var server = app.listen(8080);
+console.log('Express server started on %s:%s',server.address().address,server.address().port);
+
+/* Socket io part */
+var io = socketio.listen(server);
+
+/**
+	For ADMINS
+  */
+app.get('/admin',function(req,res){
+	/*Attendee.find({}, function (err,docs){
+		if(err)
+			console.log(err);*/
+		res.send( JSON.stringify({username:req.user/*, QAs: docs*/}));
+	/*});
+	*/
+});
---
+++ model/Patient.js
@@ -0,0 +1,102 @@
+/**
+* Glosario:
+* MH (Medical History) : Antecedente medico
+* Personal Data : Datos Filiatorios
+* alive or deceased : vivo o fallecido
+* COD (cause of death): Causa de muerte/fallecimiento
+* dbt :
+* hta : 
+* thyroid: tiroides
+* others : otros
+*/
+
+var personalDataSchema = new Schema({
+	fname: String,
+	lname: String,
+	age: Number,
+	gender: String,
+	address: String,
+	city: String
+});
+
+var PersonalData = mongoose.model('PersonalData', personalDataSchema);
+
+var parentMH = new Schema({
+	alive: Boolean,
+	/*deceased: Boolean,*/
+	cod: String,
+	dbt: Boolean,
+	hta: Boolean,
+	dislipedemia: Boolean,
+	thyroid: String,
+	chagas: Boolean,
+	others: String
+});
+
+var ParentMH = mongoose.model('ParentMH',parentMH);
+
+var personalMHschema = new Schema({
+	dbt: Boolean,
+	hta: Boolean,
+	dislipedemia: Boolean,
+	thyroid: String,
+	chagas: Boolean,
+	arhythmia: Boolean,
+	otros: String,
+	treatment: Boolean,
+	treatment2: String,
+	tabaquism: Boolean,
+	cigDay: Number,
+	years: Number,
+	abstinence: Number
+});
+
+var PersonalMH = mongoose.model('PersonalMH', personalMHschema);
+
+var physicalActivitySchema = new Schema({
+	leisure: Boolean,
+	competition: Boolean,
+	name: String,
+	freq: Number,
+	hours: Number
+});
+
+var personalFitnessSchema = new Schema({
+	now:[physicalActivitySchema],
+	previous:[physicalActivitySchema]
+});
+
+var PersonalFitness = mongoose.model('PersonalFitness', personalFitnessSchema);
+
+var vitalSignsSchema = new Schema({
+	pulseFreq: Number,
+	regular: Boolean,
+	sistolic: Number,
+	diastolic: Number
+});
+
+var anthropometricMeassuresSchema = new Schema({
+	size: Number,
+	weight: Number,
+	weist: Number,
+});
+
+var hearingSchema = new Schema({
+	r1hiperfon: Boolean,
+	r1hipofon: Boolean,
+	r1normofon: Boolean,
+	r2hiperfon: Boolean,
+	r2hipofon: Boolean,
+	r2normofon: Boolean,
+	desdoblado: Boolean,
+	dynamic: Boolean,
+	fixed: Boolean,
+	soplos: Boolean,
+	others: String,
+	pulmonarhearingNormal: Boolean,
+	pulmonarFinding: String
+});
+
+glicemiaArtherialPressure new Schema({
+	description: String
+});
